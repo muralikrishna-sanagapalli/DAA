00e4fd96f206615ccfc7ffe65752457d9fab0db3
muralikrishna-sanagapalli <muralisanagapalli2411@gmail.com>
2025-06-30 10:44:57 +0530

    Add files via upload

---
+++ #include iostream.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() 
+{
+    int num1=15;
+    int num2=25;
+    int sum=num1+num2;
+    std::cout<<"The sum of" <<num1<< "and"<<num2<< "is" <<sum<<std::endl;
+    return 0;
+}
